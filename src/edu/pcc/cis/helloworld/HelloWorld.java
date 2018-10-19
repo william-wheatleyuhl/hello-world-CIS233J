45df0fc7c5c68c212cda8fb857d2bac2dbf6fb5f
william-wheatleyuhl <william.wheatleyuhl@pcc.edu>
2018-10-18 23:45:43 -0700

    Updated Documentation

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,6 +1,10 @@
 package edu.pcc.cis.helloworld;
+
 import java.util.Date;
 
+/**
+ * Created by william-wheatleyuhl on 10/18/2018
+ */
 public class HelloWorld {
     public static void main (String[] args) {
         System.out.println("Hello, World!");
