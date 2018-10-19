154027b3b9a64b768637f81b0686f5934913b2a4
william-wheatleyuhl <william.wheatleyuhl@pcc.edu>
2018-10-18 23:38:57 -0700

    print date

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,7 +1,9 @@
 package edu.pcc.cis.helloworld;
+import java.util.Date;
 
 public class HelloWorld {
     public static void main (String[] args) {
         System.out.println("Hello, World!");
+        System.out.println(new Date());
     }
 }
